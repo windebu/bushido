4ad34203086a2cb3008433bf630ce18fc44a5b16
dreadmozart <dreadmozart@gmail.com>
2014-05-29 14:57:37 +0400

    Improve a AdminUser

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,28 +1,34 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
 
-  index do
-    selectable_column
-    id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
-    actions
-  end
+    # Permitted parameters
+    permit_params :email, :password, :password_confirmation
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
-  form do |f|
-    f.inputs "Admin Details" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+    # Displayed columns
+    index do
+        selectable_column
+        id_column
+        column :email
+        column :current_sign_in_at
+        column :last_sign_in_at
+        column :sign_in_count
+        column :created_at
+        actions
     end
-    f.actions
-  end
 
+    # Filters for email, currentSignInAt, lastSignInAt, signInCount, createdAt
+    filter :email
+    filter :current_sign_in_at
+    filter :last_sign_in_at
+    filter :sign_in_count
+    filter :created_at
+
+    # Form for Admin Details
+    form do |f|
+        f.inputs "Admin Details" do
+            f.input :email
+            f.input :password
+            f.input :password_confirmation
+        end
+        f.actions
+    end
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,22 @@
 class AdminUser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+
+	# Devise mechanism for AdminUser
+	#-----------------------------------------------------------#	
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, 
+           :recoverable, :rememberable, :trackable, :validatable
+
+    # Devise Callback sends the user a link to create a new password 
+    after_create { |admin| admin.send_reset_password_instructions }
+
+    # Method let a create a user without providing a password
+    def password_required?
+    	new_record? ? false : super
+    end
+
+    # Define for display name of user as email
+    def to_s
+  		"#{email}"
+  	end
 end
